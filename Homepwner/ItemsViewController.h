74acec671fbc323000b9ded9f918c1e881ab2d4f
Ryan Gallagher <ragfoo@yahoo.com>
2013-04-24 20:57:58 -0400

    new items viewcontrller

---
+++ Homepwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Computerlab on 4/24/13.
+//  Copyright (c) 2013 Ryan Gallagher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/ItemsViewController.m
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Computerlab on 4/24/13.
+//  Copyright (c) 2013 Ryan Gallagher. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+
+@implementation ItemsViewController
+
+@end
